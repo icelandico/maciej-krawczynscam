069ac77729e96193f4ef649087d39fe1152f072f
Michal <mail@michalmuszynski.com>
2021-10-14 18:30:43 +0200

    Calculate time left to date

--- src/components/WelcomePage/WelcomePage.js
+++ src/components/WelcomePage/WelcomePage.js
@@ -1,7 +1,46 @@
+import React, { useEffect, useState } from 'react';
 import './index.css';
-import BgImg from './../../img/bg.jpg'
+import BgImg from './../../img/bg.jpg';
+
+const calculateTimeLeft = () => {
+  const difference = +new Date('18 Oct 2021') - +new Date();
+  let timeLeft = {};
+
+  if (difference > 0) {
+    timeLeft = {
+      days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+      hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+      minutes: Math.floor((difference / 1000 / 60) % 60),
+      seconds: Math.floor((difference / 1000) % 60)
+    };
+  }
+  return timeLeft;
+}
 
 export const WelcomePage = () => {
+  const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
+  const timerComponents = [];
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setTimeLeft(calculateTimeLeft());
+    }, 1000);
+
+    return () => clearTimeout(timer);
+  });
+
+  Object.keys(timeLeft).forEach((interval) => {
+    if (!timeLeft[interval]) {
+      return;
+    }
+
+    timerComponents.push(
+        <span>
+            {timeLeft[interval]} {interval}{" "}
+          </span>
+    );
+  });
+
   return (
       <div className="main-welcome">
         <div className="main-welcome__container">
@@ -9,6 +48,9 @@ export const WelcomePage = () => {
         </div>
         <div className="main-welcome__info">
           <p className="main-welcome__info-text">Wróć i odbierz nagrodę za:</p>
+        </div>
+        <div className="main-welcome__timer">
+          {timerComponents.length ? timerComponents : <span>Time's up!</span>}
         </div>
       </div>
   );
