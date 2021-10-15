f875513dedc27598ff10f91d6cbcac7a3d46f61b
Michal <mail@michalmuszynski.com>
2021-10-15 20:36:28 +0200

    Change date

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { Form } from "./components/Form/Form";
 import Sound from './sound/bg_music.mp3';
 
 const calculateTimeLeft = () => {
-  const difference = +new Date('15 Oct 2021 19:48') - +new Date();
+  const difference = +new Date('15 Oct 2021 20:41') - +new Date();
   let timeLeft = {};
 
   if (difference > 0) {
